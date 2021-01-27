457bb67f3730418ce293f2bb584a6fb3347ff3ff
jfairch81 <55251486+jfairch81@users.noreply.github.com>
2021-01-27 16:41:00 -0500

    Update led_bash.py

--- Python/led_bash.py
+++ Python/led_bash.py
@@ -1,11 +1,11 @@
 #!/bin/bash  #run program
 
-echo "Judes LED Running" # lets u know program running
+echo "LED Running!" # easy for troubleshooting
 
 
 for i in {1..20} # Needs to turn on and off 10 times, 10 * 2 = 20
 do
 	/usr/bin/gpio -1 toggle 38 
-	sleep 1  
+	sleep 1  # break in between so you can see it easily
 done
 
